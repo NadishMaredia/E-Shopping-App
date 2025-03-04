a5f59b520b3d6a78cdc5552fcf28b52af39c641d
Nadish <nadishns97@gmail.com>
2025-03-03 21:45:27 -0500

    Add getter setter

--- shopper/src/main/java/com/eshop/shopper/model/Category.java
+++ shopper/src/main/java/com/eshop/shopper/model/Category.java
@@ -17,5 +17,20 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long categoryId;
     private String categoryName;
-    //adding comments to test yml
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
