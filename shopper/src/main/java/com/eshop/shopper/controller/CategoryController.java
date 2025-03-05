9af304d6e634a595612fbe500fb5609c6ec22b91
Nadish <nadishns97@gmail.com>
2025-03-05 14:32:15 -0500

    Add validation and exceptions for category api

--- shopper/src/main/java/com/eshop/shopper/controller/CategoryController.java
+++ shopper/src/main/java/com/eshop/shopper/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.eshop.shopper.controller;
 import com.eshop.shopper.implementation.CategoryService;
 import com.eshop.shopper.model.Category;
 import com.eshop.shopper.service.ICategoryService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,28 +28,20 @@ public class CategoryController {
     }
 
     @PostMapping("/api/public/category")
-    public ResponseEntity<String> createCategory(@RequestBody Category category) {
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category) {
         this.categoryService.createCategory(category);
         return new ResponseEntity<>("Category Added", HttpStatus.CREATED);
     }
 
     @DeleteMapping("/api/admin/category/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable()  Long categoryId) {
-        try {
-            String status = this.categoryService.deleteCategory(categoryId);
-            return ResponseEntity.ok(status);
-        } catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+        String status = this.categoryService.deleteCategory(categoryId);
+        return ResponseEntity.ok(status);
     }
 
     @PutMapping("/api/public/category/{categoryId}")
-    public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId) {
-        try {
-            Category status = this.categoryService.updateCategory(category, categoryId);
-            return ResponseEntity.ok("Category with id: " +categoryId +" updated");
-        } catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+    public ResponseEntity<String> updateCategory(@Valid @RequestBody Category category, @PathVariable Long categoryId) {
+        Category status = this.categoryService.updateCategory(category, categoryId);
+        return ResponseEntity.ok("Category with id: " +categoryId +" updated");
     }
 }
--- shopper/src/main/java/com/eshop/shopper/repository/ICategoryRepository.java
+++ shopper/src/main/java/com/eshop/shopper/repository/ICategoryRepository.java
@@ -4,4 +4,5 @@ import com.eshop.shopper.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ICategoryRepository extends JpaRepository<Category, Long> {
+    Category findByCategoryName(String categoryName);
 }
