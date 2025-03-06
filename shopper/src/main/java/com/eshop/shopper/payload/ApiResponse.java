6502b24cccc0aaecbb301024d9e3ec2458754bcd
Nadish <nadishns97@gmail.com>
2025-03-06 15:09:10 -0500

    Add constructor

--- shopper/src/main/java/com/eshop/shopper/payload/ApiResponse.java
+++ shopper/src/main/java/com/eshop/shopper/payload/ApiResponse.java
@@ -5,9 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class ApiResponse {
     public String message;
     private boolean status;
+
+    public ApiResponse(String message, boolean status) {
+        this.message = message;
+        this.status = status;
+    }
 }
