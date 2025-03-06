6e6b8f15fd812a1e82f2806d2195e27357450b44
Nadish <nadishns97@gmail.com>
2025-03-06 15:11:42 -0500

    Add getter setter

--- shopper/src/main/java/com/eshop/shopper/payload/CategoryResponse.java
+++ shopper/src/main/java/com/eshop/shopper/payload/CategoryResponse.java
@@ -16,4 +16,52 @@ public class CategoryResponse {
     private Long totalElements;
     private Integer totalpages;
     private boolean lastPage;
+
+    public List<CategoryDTO> getContent() {
+        return content;
+    }
+
+    public void setContent(List<CategoryDTO> content) {
+        this.content = content;
+    }
+
+    public Integer getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(Integer pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(Long totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public Integer getTotalpages() {
+        return totalpages;
+    }
+
+    public void setTotalpages(Integer totalpages) {
+        this.totalpages = totalpages;
+    }
+
+    public boolean isLastPage() {
+        return lastPage;
+    }
+
+    public void setLastPage(boolean lastPage) {
+        this.lastPage = lastPage;
+    }
 }
